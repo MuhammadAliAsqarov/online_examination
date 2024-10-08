4456ecb5a6884b3268da7c791f38eca60b544150
MukhammadAli <rasulovadildora3@gmail.com>
2024-10-08 16:00:19 +0500

    added requirements.txt, some apps authentication, exam

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
-    path('exam/', include('exams.urls'))
+    path('exam/', include('exam.urls'))
 ]
--- exam/models.py
+++ exam/models.py
@@ -1,33 +1,26 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from rest_framework.authtoken.admin import User
 
-class User(AbstractUser):
-    is_teacher = models.BooleanField(default=False)
-    is_student = models.BooleanField(default=False)
+Question_choices = (
+    (1, 'MCQ'),
+    (2, 'Open')
+)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
 
 
 class Test(models.Model):
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tests')
-    title = models.CharField(max_length=255)
-    total_time = models.DurationField()
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=128)
+    questions = models.TextField(max_length=128)
+    deadline = models.DateField()
+    time_limit = models.IntegerField(default=30)
 
-class Question(models.Model):
-    test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='questions')
-    text = models.TextField()
-    is_multiple_choice = models.BooleanField(default=False)
-
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
-    text = models.CharField(max_length=255)
-    is_correct = models.BooleanField(default=False)
-
-class ExamSession(models.Model):
-    test = models.ForeignKey(Test, on_delete=models.CASCADE)
-    student = models.ForeignKey(User, on_delete=models.CASCADE)
-    start_time = models.DateTimeField(auto_now_add=True)
-    end_time = models.DateTimeField(null=True, blank=True)
-
-class Result(models.Model):
-    exam_session = models.OneToOneField(ExamSession, on_delete=models.CASCADE)
-    score = models.FloatField()
-    evaluated = models.BooleanField(default=False)
+    def __str__(self):
+        return self.title
